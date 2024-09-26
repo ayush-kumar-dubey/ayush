93a89fef352c69c0f0c94fe46af136703da106b2
ayush kuamr dubey <ayushdubey13032001@gmail.com>
2024-09-26 19:57:31 +0530

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,11 @@
+const canvas =document . querySelector("canvas");
+const tooloptions = document . querySelectorAll("tools-board.tool")
+const fillcolorchekbox =document . querySelector("brush-size-slider")
+
+
+
+
+ let currentstep = 0;
+ let isDiawing = false;
+ let selecedttool
+ 
